0b45ac723b8bc00fa46093aa7487b4808691ea03
Timm49 <timvanengelen@gmail.com>
2025-06-02 01:17:38 +0200

    Cleaup works ai think

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,13 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Timm49\SimilarContentLaravel\Attributes\HasEmbeddings;
-use Timm49\SimilarContentLaravel\Traits\HasSimilarContent;
 
-#[HasEmbeddings]
 class Article extends Model
 {
-    use HasFactory, SoftDeletes, HasSimilarContent;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'title',
@@ -34,14 +31,5 @@ class Article extends Model
     public function getRouteKeyName()
     {
         return 'slug';
-    }
-
-    public function getEmbeddingData(): string
-    {
-        $embedData = collect($this->getFillable())
-            ->mapWithKeys(fn ($key) => [$key => $this->getAttribute($key)])
-            ->toArray();
-
-        return json_encode($embedData);
     }
 }
